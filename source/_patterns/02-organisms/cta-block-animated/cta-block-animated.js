850189f7ae9c15e02d8c65e794399f0df934dd4a
michelledarling <michelle.darling@hook42.com>
2020-10-30 14:49:46 -0500

    homepage components first pass

--- source/_patterns/02-organisms/cta-block-animated/cta-block-animated.js
+++ source/_patterns/02-organisms/cta-block-animated/cta-block-animated.js
@@ -2,9 +2,9 @@
 	$(document).ready(function() {
 		$(window).scroll( function(){
 			$('.cta-block-animated').each( function(){
-				var bottom_of_element = $(this).offset().top + $(this).outerHeight();
-				var bottom_of_window = $(window).scrollTop() + $(window).height();
-				if( bottom_of_window > bottom_of_element ){
+				var elemTop = $(this).offset().top + 300;
+				var viewBottom = $(window).scrollTop() + $(window).height();
+				if( elemTop < viewBottom ){
 					var targetLeft = $(this).find('.animate-left');
 					var targetRight = $(this).find('.animate-right');
 					$(targetLeft).animate({
