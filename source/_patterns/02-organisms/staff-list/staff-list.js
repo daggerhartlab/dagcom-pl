64c9be9d92f11ef7e7c3af97531e77e787e7ebd9
michelledarling <michelle.darling@hook42.com>
2020-12-01 13:15:15 -0600

    add global function to test if element is in viewport  and update all components to use it

--- source/_patterns/02-organisms/staff-list/staff-list.js
+++ source/_patterns/02-organisms/staff-list/staff-list.js
@@ -1,18 +1,16 @@
 (function($) {
 	$(document).ready(function() {
-		$('.staff-card').each(function() {
-			$(this).css('opacity', 0);
-		})
-		$(window).scroll( function(){
-			$('.staff-list').each( function(){
-				var elemTop = $(this).offset().top + 300;
-				var viewBottom = $(window).scrollTop() + $(window).height();
-				if (elemTop < viewBottom) {
-					$('.staff-card').each(function(index) {
-						$(this).addClass('animate-0');
-					});
-				}
-			});
+		if (!($('.staff-list .card-wrapper').isInViewport())) {
+			$('.staff-list .staff-card').css('opacity',0);
+		}
+	});
+	$(window).scroll( function(){
+		$('.staff-list').each( function(){
+			if ($(this).isInViewport()) {
+				$('.staff-list .staff-card').each(function(index) {
+					$(this).addClass('animate-' + index);
+				});
+			}
 		});
 	});
 })(jQuery);
